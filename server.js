10e2b477538fe83b5376ffb74c700191f9325e98
JahanzaibBokhari <97527112+JahanzaibBokhari@users.noreply.github.com>
2024-03-19 23:53:16 -0400

    Update server.js

--- server.js
+++ server.js
@@ -62,62 +62,6 @@ app.post("/api/user/login", (req, res) => {
         });
 });
 
-app.get("/api/user/favourites", passport.authenticate('jwt', { session: false }), (req, res) => {
-    userService.getFavourites(req.user._id)
-        .then(data => {
-            res.json(data);
-        }).catch(msg => {
-            res.status(422).json({ error: msg });
-        })
-
-});
-
-app.put("/api/user/favourites/:id", passport.authenticate('jwt', { session: false }), (req, res) => {
-    userService.addFavourite(req.user._id, req.params.id)
-        .then(data => {
-            res.json(data)
-        }).catch(msg => {
-            res.status(422).json({ error: msg });
-        })
-});
-
-app.delete("/api/user/favourites/:id", passport.authenticate('jwt', { session: false }), (req, res) => {
-    userService.removeFavourite(req.user._id, req.params.id)
-        .then(data => {
-            res.json(data)
-        }).catch(msg => {
-            res.status(422).json({ error: msg });
-        })
-});
-
-app.get("/api/user/history", passport.authenticate('jwt', { session: false }), (req, res) => {
-    userService.getHistory(req.user._id)
-        .then(data => {
-            res.json(data);
-        }).catch(msg => {
-            res.status(422).json({ error: msg });
-        })
-
-});
-
-app.put("/api/user/history/:id", passport.authenticate('jwt', { session: false }), (req, res) => {
-    userService.addHistory(req.user._id, req.params.id)
-        .then(data => {
-            res.json(data)
-        }).catch(msg => {
-            res.status(422).json({ error: msg });
-        })
-});
-
-app.delete("/api/user/history/:id", passport.authenticate('jwt', { session: false }), (req, res) => {
-    userService.removeHistory(req.user._id, req.params.id)
-        .then(data => {
-            res.json(data)
-        }).catch(msg => {
-            res.status(422).json({ error: msg });
-        })
-});
-
 userService.connect()
     .then(() => {
         app.listen(HTTP_PORT, () => { console.log("API listening on: " + HTTP_PORT) });
